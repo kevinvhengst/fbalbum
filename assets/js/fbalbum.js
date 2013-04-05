3abd5c3ca315be2583ec25e75580368b91d994a1
kevinvhengst <kevinvanhengst1992@live.nl>
2013-04-05 12:55:50 +0530

    version 1.0

---
+++ assets/js/fbalbum.js
@@ -0,0 +1,35 @@
+//paste you fb-company album URL here. 
+var album_url = 'ENTER FB-PAGE ALBUM URL HERE',
+uid = album_url.split('?')[1].split('.')[1];
+$(document).ready(function () {
+// Ajax Request to Facebook Graph API
+var req_url = 'http://graph.facebook.com/'+uid+'/photos';
+
+$.ajax({
+    type: "GET",
+    url: req_url,
+    success: function(res) {
+        data = res
+        generateImg(data);
+    },
+    error: function() {
+      $('.plugin_fbalbum').append('<h2> No valid image response.</h2>')
+    }
+});
+
+//Add image in the placeholder
+var generateImg = function (data) {
+    var len = data.data.length, image_stack = '', small_image_url, big_image_url,img;
+    for(i = 0 ; i < len; i++) {
+        small_image_url = data.data[i].images[8].source;
+        big_image_url = data.data[i].images[1].source;
+        img = '<a href="'+big_image_url+'" class="group1" > '+'<img src="'+small_image_url+'" class="small" ' +' /></a>';
+        image_stack += img;
+    }
+    $(".plugin_fbalbum").append(image_stack);
+    //Add Colorbox affect.
+    $(document).ready(function () {
+       $(".group1").colorbox({rel:'group3', transition:"fade", width:"75%", height:"75%"});
+    });
+}
+});
